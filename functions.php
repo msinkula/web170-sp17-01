7f836cfdb4f81e8d2d11f9b933533553fc91a3bb
Mike Sinkula <mike@mikesinkula.com>
2017-04-24 14:30:53 -0700

    Creating a WordPress Theme From a Template

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php 
+
+/*
+Theme Name: WordPress Spring 2017 Section 01 Demo
+Author: Mike Sinkula
+Author URI: http://mikesinkula.com/
+Description: This is the demo theme for the WEB170 section 01 class for the Spring 2017 Quarter.
+Version: 42.0
+*/
+
+
+?>
